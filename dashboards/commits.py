a00a674ed24fd78977b85ce29756749667d87a2b
Evan K <ekcopersonal@gmail.com>
2024-08-02 17:57:41 -0400

    feat: update bar chart with bid volumes

--- dashboards/commits.py
+++ dashboards/commits.py
@@ -1,5 +1,7 @@
 import dash
+import plotly.express as px
 import polars as pl
+
 from dash import dcc, html, dash_table, Input, Output
 from lancedb_tables.lance_table import LanceTable
 
@@ -17,11 +19,24 @@ commit_df = (
     pl.from_arrow(commitments_table.to_lance().to_table())
     # calculate bid latency
     .with_columns(
-        (pl.col('dispatchTimestamp') - \
+        (pl.from_epoch('dispatchTimestamp',
+                       time_unit='ms')).dt.round('1s').alias('datetime'),
+        (pl.col('dispatchTimestamp') -
          pl.col('decayStartTimeStamp')).alias('bid_latency'),
         (pl.col('bid')/10**18).alias('bid_eth'),
-        (pl.from_epoch('dispatchTimestamp',
-         time_unit='ms')).dt.round('1s').alias('datetime')
+        (pl.col('decayEndTimeStamp') -
+         pl.col('decayStartTimeStamp')).alias('total_decay_range'),
+        # if decay_time_past is negative, it means that the provider commited outside of the decay range so the bid would have fully decayed to 0.
+        (pl.col('decayEndTimeStamp') -
+         pl.col('dispatchTimestamp')).alias('decay_time_past'),
+    )
+    .with_columns(
+        # total decay time is the range [beginDecayTimestamp, endDecayTimestamp] and `decay_time_past` is the time the provider commited to the transaction.
+        (1 - (pl.col('decay_time_past') / pl.col('total_decay_range'))
+         ).alias('decay_percent')
+    )
+    .with_columns(
+        (pl.col('bid') * pl.col('decay_percent') / 10**18).alias('decayed_bid_eth'),
     )
     .sort(by='datetime', descending=True)
 ).rename({
@@ -34,7 +49,35 @@ commit_df = (
     'bid_eth',
     'bid_latency',
     'txnHash',
+    'isSlash',
+    'decayed_bid_eth',
     # 'dispatchTimestamp',
+)
+
+# undecayed bid vol
+total_bids = commit_df.with_columns(pl.col('datetime').dt.round('1d')).group_by('datetime').agg(
+    pl.sum('bid_eth').alias('total_bids'),
+)
+
+# decayed bid vol
+total_decayed_bids = commit_df.filter(pl.col('decayed_bid_eth') > 0).with_columns(pl.col('datetime').dt.round('1d')).group_by('datetime').agg(
+    pl.sum('decayed_bid_eth').alias('total_decayed_bids_eth'),
+)
+
+bid_volume_df = total_bids.join(total_decayed_bids, on='datetime', how='inner')
+
+# Convert to Pandas DataFrame for Plotly
+bid_volume_df_pd = bid_volume_df.to_pandas()
+
+
+# Create the bar chart
+fig = px.bar(
+    bid_volume_df_pd,
+    x='datetime',
+    y=['total_bids', 'total_decayed_bids_eth'],
+    barmode='group',
+    labels={'datetime': 'DateTime', 'value': 'ETH'},
+    title='Total Bids and Decayed Bids Over Time'
 )
 
 # Initialize the Dash app
@@ -42,6 +85,7 @@ app = dash.Dash(__name__)
 
 # Define the layout of the app
 app.layout = html.Div([
+    dcc.Graph(figure=fig),
     html.H6("Bidder Latency: Enter bidder address below"),
     dcc.Input(
         id="bidder-input",
@@ -52,27 +96,7 @@ app.layout = html.Div([
         id='table',
         columns=[{"name": i, "id": i} for i in commit_df.columns]
     )
-]
-)
+])
 
-# Define the callback to update the table based on the input value
-
-
-@app.callback(
-    Output('table', 'data'),
-    [Input('bidder-input', 'value')]
-)
-def update_table(bidder_addr):
-    if bidder_addr:
-        # DF used to calculate bid latency
-        df = (commit_df
-              .filter(
-                  pl.col('bidder') == bidder_addr.lower())
-              )
-        return df.to_dicts()
-    return []
-
-
-# Run the app
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8050, debug=True)
+    app.run_server(debug=True)
